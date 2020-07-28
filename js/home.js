becac3eb98210457bd9574e3868b9aac9123b5b4
Lynn Nardozzi <sweetxdrops@gmail.com>
2020-07-28 01:54:25 -0300

    Carga afiliados y noticias externas

--- js/home.js
+++ js/home.js
@@ -3,6 +3,14 @@ $(document).ready(function iniciaTodo() {
         document.getElementsByClassName("news-latest")[0].innerHTML = estado;
     });
 
+    $.get("https://raw.githubusercontent.com/GardieMaker/data/master/status/news", function(noticias, success, xhr) {
+        document.getElementById("news-list").innerHTML = noticias;
+    });
+
+    $.get("https://raw.githubusercontent.com/GardieMaker/data/master/status/affiliates", function(afiliados, success, xhr) {
+        document.getElementById("footer-links").innerHTML = afiliados;
+    });
+
 });
 
 $(function() { 
