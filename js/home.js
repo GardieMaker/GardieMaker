af4911c8e46f47e89df8c3a4a569ef2d70405636
Zunay <sweetxdrops@gmail.com>
2020-10-08 22:15:59 -0300

    noticias automatizadas

--- js/home.js
+++ js/home.js
@@ -3,15 +3,55 @@ $(document).ready(function iniciaTodo() {
         document.getElementsByClassName("news-latest")[0].innerHTML = estado;
     });
 
-    $.get("https://raw.githubusercontent.com/GardieMaker/data/master/status/news", function(noticias, success, xhr) {
-        document.getElementById("news-list").innerHTML = noticias;
-    });
-
     $.get("https://raw.githubusercontent.com/GardieMaker/data/master/status/affiliates", function(afiliados, success, xhr) {
         document.getElementById("footer-links").innerHTML = afiliados;
     });
 
 });
+
+function get(blogger) {
+    // Obtener los tres últimos posts [ Fecha + Categoría + Título ]
+    var entradas = 0, html = "", titulo, fecha, url;
+
+    for (post = 0; post < blogger.feed.entry.length; post++) {
+        if (entradas == 3) break;
+        
+        for (tag = 0; tag < blogger.feed.entry[post].category.length; tag++) {
+            var categoria = blogger.feed.entry[post].category[tag].term;
+            // Categorías [ anuncios, changelog, featured, updates, actividades ]
+
+            if (categoria == "anuncios" || categoria == "changelog" || categoria == "featured" || categoria == "updates" || categoria == "actividades" ) {
+                // Post válido
+                switch (categoria) {
+                    case "anuncios":titulo = "[ANUNCIO] ";break;
+                    case "changelog":titulo = "[CHANGELOG] ";break;
+                    case "featured":titulo = "[PORTADA] ";break;
+                    case "updates":titulo = "[ACTUALIZACIÓN] ";break;
+                    case "actividades":titulo = "[ACTIVIDAD] ";break;
+                    default: titulo = "";
+                };
+
+                if (titulo != "") {
+                    // Buscar titulo y fecha
+                    titulo = titulo + blogger.feed.entry[post].link[4].title;
+                    fecha = blogger.feed.entry[post].published.$t;
+                    fecha = fecha.slice(8,10) + "/" + fecha.slice(5,7);
+                    url = blogger.feed.entry[post].link[4].href;
+
+                    html = html + '<div class="news"><div class="date">' + fecha
+                                + '</div><div class="text"><a href="' + url
+                                + '">' + titulo + '</a></div></div>';
+                    entradas++;
+                    break;
+                };
+            };
+        };
+    };
+
+    $(document).ready(function () {
+        document.getElementById("news-list").innerHTML = html;
+    });
+};
 
 $(function() { 
     $("#load-code").click(function() { 
